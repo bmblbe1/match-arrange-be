5b3aa770bb075f8a2cddd5ee2db26ccef7f318af
bmblbe1 <paolo.peloza@gmail.com>
2024-07-28 17:02:21 +0200

    try again

--- api/index.js
+++ api/index.js
@@ -2,6 +2,10 @@ import createError from 'http-errors';
 import express from 'express';
 
 import groupsRouter from './groups.js';
+import pkg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+const { Pool } = pkg;
 
 var app = express();
 
@@ -23,8 +27,16 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(443, () => {
-  console.log('listening');
+app.use('/groups', function(req, res, next) {
+  console.log('ovdje sam');
+  pool.query('SELECT * FROM "groups"', (error, results) => {
+    if (error) {
+      console.log(error, 'ovdje')
+      return res.status(500).json({ error: error.message });
+    } else {
+      res.json({ groups: results.rows });
+    }
+  });
 })
 
 export default app;
