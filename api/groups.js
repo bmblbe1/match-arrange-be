fb7a978f4c756753bb112527cb8eef50dca25e2b
bmblbe1 <paolo.peloza@gmail.com>
2024-07-28 16:50:38 +0200

    add console.log

--- api/groups.js
+++ api/groups.js
@@ -8,6 +8,7 @@ router.get('/', function(req, res, next) {
   console.log('ovdje sam');
   pool.query('SELECT * FROM "groups"', (error, results) => {
     if (error) {
+      console.log(error, 'ovdje')
       return res.status(500).json({ error: error.message });
     } else {
       res.json({ groups: results.rows });
