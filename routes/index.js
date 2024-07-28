4cb5377406c7a2fff90b8b6a09d5383225ff1794
bmblbe1 <paolo.peloza@gmail.com>
2024-07-28 16:32:13 +0200

    console log debug

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ import { pool } from '../db.js';
 
 
 router.get('/groups', function(req, res, next) {
+  console.log('ovdje sam');
   pool.query('SELECT * FROM "groups"', (error, results) => {
     if (error) {
       return res.status(500).json({ error: error.message });
